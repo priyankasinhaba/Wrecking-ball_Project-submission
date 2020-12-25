a658dd84ea5b2d692780a61ab706c1f892feaa0e
priyankasinhaba <73702702+priyankasinhaba@users.noreply.github.com>
2020-12-25 15:21:44 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,35 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':1,
+          'friction':0.3,
+          'density':0.1,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+      
+    }
+    display(){
+        if (this.body.speed <10){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(2);
+      stroke("red");
+      fill("orange");
+      rect(0, 0, this.width, this.height);
+      pop();
+        }
+        else {
+            World.remove (world, this.body);
+           
+        }
+      console.log(this.body.speed);
+    }
+  }
